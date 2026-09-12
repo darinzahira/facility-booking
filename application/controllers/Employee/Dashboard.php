d112d1d18f0d7cde58f469bc63f5b3558b5e291a
darinzahira <zahiradarin@gmail.com>
2026-09-12 14:22:19 +0700

    comment controller dashboard

--- application/controllers/Employee/Dashboard.php
+++ application/controllers/Employee/Dashboard.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller
 {
+    //ini construct
     public function __construct()
     {
         parent::__construct();
